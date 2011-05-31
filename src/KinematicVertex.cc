d419ec99be8990b595f2dc2fc06f9186c35b0ef2
cmsbuild <>
2011-05-31 14:11:28 +0000

    This commit was manufactured by cvs2git to create branch 'RecoVertex- KinematicFitPrimitives-fixmemleak'.
    Sprout from master 2011-05-31 14:10:20 UTC Frank Meier <sha1-cbef2ad636c62802bd9545d5d36a8875981b19b2@cern.ch> 'Fixed memory leak. See https://hypernews.cern.ch/HyperNews/CMS/get/code-perf/558/1/1/1.html'
    Cherrypick from master 2010-08-03 00:01:41 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Add std:: namespace qualifiers':
        src/KinematicVertex.cc

--- src/KinematicVertex.cc
+++ src/KinematicVertex.cc
@@ -2,6 +2,8 @@
 #include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticle.h"
 #include "RecoVertex/KinematicFitPrimitives/interface/KinematicTree.h"
 #include "RecoVertex/KinematicFitPrimitives/interface/TransientTrackKinematicParticle.h"
+#include "TrackingTools/TransientTrack/interface/TrackTransientTrack.h"
+#include "TrackingTools/TransientTrack/interface/GsfTransientTrack.h"
 
 KinematicVertex::KinematicVertex()
 {vl = false;}
@@ -47,8 +49,8 @@ bool KinematicVertex::operator==(const KinematicVertex& other)const
  {
   GlobalPoint cPos = this->position();
   GlobalPoint oPos = other.position();
-  AlgebraicMatrix33 const & cCov = this->error().matrix();
-  AlgebraicMatrix33 const & oCov = other.error().matrix();
+  AlgebraicMatrix cCov = this->error().matrix();
+  AlgebraicMatrix oCov = other.error().matrix();
   if((cPos.x()==oPos.x())&&(cPos.y()==oPos.y())&&(cPos.z()==oPos.z())
                                                       &&(cCov==oCov))
   res = true;
@@ -123,11 +125,20 @@ KinematicVertex::operator reco::Vertex()
 
   for (std::vector<RefCountedKinematicParticle>::const_iterator i = daughters.begin();
        i != daughters.end(); ++i) {
+
     const TransientTrackKinematicParticle * ttkp = dynamic_cast<const TransientTrackKinematicParticle * >(&(**i));
     if(ttkp != 0) {
-      reco::TrackBaseRef tr = ttkp->initialTransientTrack()->trackBaseRef();
-      if (tr.isNonnull())
-	vertex.add(tr, ttkp->refittedTransientTrack().track(), 1.);
+      const reco::TrackTransientTrack * ttt = dynamic_cast<const reco::TrackTransientTrack*>(ttkp->initialTransientTrack()->basicTransientTrack());
+      if ((ttt!=0) && (ttt->persistentTrackRef().isNonnull())) {
+	reco::TrackRef tr = ttt->persistentTrackRef();
+	vertex.add(reco::TrackBaseRef(tr), ttkp->refittedTransientTrack().track(), 1.);
+      } else {
+	const reco::GsfTransientTrack * ttt = dynamic_cast<const reco::GsfTransientTrack*>(ttkp->initialTransientTrack()->basicTransientTrack());
+	if ((ttt!=0) && (ttt->persistentTrackRef().isNonnull())) {
+	  reco::GsfTrackRef tr = ttt->persistentTrackRef();
+	  vertex.add(reco::TrackBaseRef(tr), ttkp->refittedTransientTrack().track(), 1.);
+	}
+      }
     }
   }
   return vertex;
